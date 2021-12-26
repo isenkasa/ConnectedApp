1d3f675cbbedea21024373049a9794b29c701047
Isen <isenkasa@gmail.com>
2021-12-26 15:25:40 -0600

    Issue #1 - Add Firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyC_YGsorC1AKAcTVy7Bd8z5nQOXX4gX_nY",
+    authDomain: "connectedapp-60158.firebaseapp.com",
+    projectId: "connectedapp-60158",
+    storageBucket: "connectedapp-60158.appspot.com",
+    messagingSenderId: "673111974342",
+    appId: "1:673111974342:web:63f673f18c36bd3c0792c7",
+    measurementId: "G-4EGG39892Q"
+  }
 };
