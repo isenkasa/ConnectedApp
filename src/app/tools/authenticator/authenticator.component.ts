b8ee02c62cd5e162f3f8d4195ee35854b34bd6f1
Isen <isenkasa@gmail.com>
2021-12-26 17:35:34 -0600

    Issue #3 - Add Login Component

---
+++ src/app/tools/authenticator/authenticator.component.ts
@@ -0,0 +1,55 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-authenticator',
+  templateUrl: './authenticator.component.html',
+  styleUrls: ['./authenticator.component.css']
+})
+export class AuthenticatorComponent implements OnInit {
+  state = AuthenticatorCompState.LOGIN;
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onForgotPasswordClick() {
+    this.state = AuthenticatorCompState.FORGOT_PASSWORD;
+  }
+
+  onCreateAccountClick() {
+    this.state = AuthenticatorCompState.REGISTER;
+  }
+
+  onLoginClick() {
+    this.state = AuthenticatorCompState.LOGIN;
+  }
+
+  isLoginState() {
+    return this.state == AuthenticatorCompState.LOGIN;
+  }
+
+  isRegisterState() {
+    return this.state == AuthenticatorCompState.REGISTER;
+  }
+
+  isForgotPasswordState() {
+    return this.state == AuthenticatorCompState.FORGOT_PASSWORD;
+  }
+
+  getStateText() {
+    switch(this.state) {
+      case AuthenticatorCompState.LOGIN:
+        return "Login";
+      case AuthenticatorCompState.REGISTER:
+        return "Register";
+      case AuthenticatorCompState.FORGOT_PASSWORD:
+        return "Forgot Password";
+    }
+  }
+}
+
+export enum AuthenticatorCompState {
+  LOGIN,
+  REGISTER,
+  FORGOT_PASSWORD
+}
